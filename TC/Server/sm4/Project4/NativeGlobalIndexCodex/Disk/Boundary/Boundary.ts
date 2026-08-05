3c4c042e383b4b5a27424f89910de40dd3fc949c
jungkeun kim <et16kr@gmail.com>
2026-08-05 19:42:03 +0900

    Add 65-participant global index cases

--- TC/Server/sm4/Project4/NativeGlobalIndexCodex/Disk/Boundary/Boundary.ts
+++ TC/Server/sm4/Project4/NativeGlobalIndexCodex/Disk/Boundary/Boundary.ts
@@ -1,6 +1,7 @@
 TestSuiteDescription  = Native Global Disk Index Boundary SourceOnly
 ###############################################################################
 singleAndManyPartitions.tc
+participantCount65.tc
 globalIndexCount64.tc
 globalIndexCount65Reject.tc
 dropCascade64.tc
--- TC/Server/sm4/Project4/NativeGlobalIndexCodex/Memory/Boundary/Boundary.ts
+++ TC/Server/sm4/Project4/NativeGlobalIndexCodex/Memory/Boundary/Boundary.ts
@@ -1,6 +1,7 @@
 TestSuiteDescription  = Native Global Memory Index Boundary SourceOnly
 ###############################################################################
 singleAndManyPartitions.tc
+participantCount65.tc
 globalIndexCount64.tc
 globalIndexCount65Reject.tc
 dropCascade64.tc
